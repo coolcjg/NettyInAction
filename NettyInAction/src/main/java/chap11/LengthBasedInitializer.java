45364837b0765b669e99bb07dc0773beb5c23117
cjg <cjg@DESKTOP-HITIFPH>
2020-05-26 23:07:02 +0900

    웹소켓

---
+++ NettyInAction/src/main/java/chap11/LengthBasedInitializer.java
@@ -0,0 +1,27 @@
+package chap11;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+public class LengthBasedInitializer extends ChannelInitializer<Channel>{
+	
+	@Override
+	protected void initChannel(Channel ch) throws Exception{
+		ChannelPipeline pipeline = ch.pipeline();
+		pipeline.addLast(new LengthFieldBasedFrameDecoder(64*1024, 0 , 8));
+		pipeline.addLast(new FrameHandler());
+	}
+	
+	public static final class FrameHandler extends SimpleChannelInboundHandler<ByteBuf>{
+		@Override
+		public void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception{
+			//프레임 작업
+		}
+	}
+
+}
